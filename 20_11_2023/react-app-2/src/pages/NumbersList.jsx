594e76de1949569db5c02bf6fb5da1e77d847650
manhal18 <manhalalheralhaluf@gmail.com>
2023-11-21 18:56:06 +0300

    react02

--- 20_11_2023/react-app-2/src/pages/NumbersList.jsx
+++ 20_11_2023/react-app-2/src/pages/NumbersList.jsx
@@ -2,20 +2,43 @@ import React, { useState } from 'react'
 
 export default function NumbersList(){
     let [numbers, setNumbers] = useState([])
+    let [numbers2, setNumbers2] = useState([1, 5, 6, 9, 3, 18, 2, 20, 10, 11, 30, 41, 50, 62, 70, 88, 77, 90, 92, 99])
+
     function add(){
-        var randomNumber = Math.floor(Math.random() * 100) + 1
+        let randomNumber = Math.floor(Math.random() * 100) + 1
         if(!numbers.includes(randomNumber)){
             setNumbers(prev => [...prev, randomNumber])
         }
     }
+
+    function remove(){
+        if(numbers2.length > 0){
+            let randomNumber = Math.floor(Math.random() * numbers2.length) 
+            console.log(randomNumber);
+            setNumbers2(prev => prev.slice(0,randomNumber).concat(prev.slice(randomNumber+1)))
+        }
+    }
     return (
         <>
-            <button onClick={add}>Add</button>
-            <ul>
-                {
-                    numbers.map(item => <li>{item}</li>) 
-                }
-            </ul>
+            <div style={{float: "left", marginRight: "400px"}}>
+                <button onClick={add}>Add</button>
+                <ul>
+                    {
+                        numbers.map((item, i) => <li key={i}>{item}</li>) 
+                    }
+                </ul>
+            </div>
+
+            <hr style={{width: "1px", height: "600px", float: "left"}}></hr>
+
+            <div style={{float: "left", marginLeft: "100px"}}>
+                <button onClick={remove}>Remove</button>
+                <ul>
+                    {
+                        numbers2.map((item, i) => <li key={i}>{item}</li>) 
+                    }
+                </ul>
+            </div>
         </>
     )
 }
